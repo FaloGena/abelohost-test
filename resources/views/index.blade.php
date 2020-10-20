6e8916a4fd6cf32ef544cf1defbd35e96191e836
FaloGena <18burg78@gmail.com>
2020-10-20 05:52:45 +0300

    Footer, index page for guests

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.main')
 @section('content')
-
+    @guest
+        <div class="not-logged-main">
+            <div class="big-name text-center">
+                <span>Exchequer</span>
+            </div>
+            <div class="need-login text-center">
+                <span>You have to <a href="#" data-toggle="modal" data-target="#loginModal">Log in</a> or <a
+                        href="/register">create an account</a> to start using the service!</span>
+            </div>
+        </div>
+    @endguest
 @endsection
